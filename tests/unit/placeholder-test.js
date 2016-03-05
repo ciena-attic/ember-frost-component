87ba498ea2247cdfa5f6fc7eee82ef8a1e80e942
Matthew Dahl <mdahl@ciena.com>
2016-03-04 18:51:46 -0800

    Switch to mocha and eslint

---
+++ tests/unit/placeholder-test.js
@@ -0,0 +1,9 @@
+import {describe, it} from 'mocha'
+
+const expect = chai.expect
+
+describe('placeholder', function () {
+  it('keeps tests from failing', function () {
+    expect(true).to.be.truthy
+  })
+})
